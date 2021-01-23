2bee0bed088e05a0360ea852f6818c9e8be73683
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-01-23 11:36:37 +0100

    added PlayCardCommand

--- src/test/scala/de/htwg/se/gwent/controller/controllerComponent/controllerBaseImpl/PlayCardCommandspecs.scala
+++ src/test/scala/de/htwg/se/gwent/controller/controllerComponent/controllerBaseImpl/PlayCardCommandspecs.scala
@@ -1,21 +1,61 @@
 package de.htwg.se.gwent.controller.controllerComponent.controllerBaseImpl
 
+import de.htwg.se.gwent.model.fieldComponent.fieldBaseImpl.{Card, Field, HandCard}
+import de.htwg.se.gwent.model.fieldComponent.fieldBaseImpl.WeatherState.Sunshine
+import de.htwg.se.gwent.model.playerComponent.Player
+import de.htwg.se.gwent.model.playerComponent.PlayerType.{BOT, TOP}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class PlayCardCommandspecs extends AnyWordSpec with Matchers {
   "is a Command Pattern to do, undo and redo Steps" when {
     "PlayCardCommand is used it" should {
-      "do the step" in {
+      val playerTop = Player(TOP,"Adrian",HandCard(Vector[Card](Card("Test",0,1,1))),0)
+      val playerBot = Player(BOT,"Stefan",HandCard(Vector[Card](Card("Test",0,1,1))),0)
+      "be able to do,undo and redo Steps from the Top Player" in {
+        val controller = new Controller(Field(Vector[Vector[Option[Card]]](),new Sunshine,playerTop,playerBot,0,0))
+        val command = new PlayCardCommand(controller.field,0,0,TOP,0,controller)
 
+        controller.field.isEmpty(0,0) should be (true)
+        controller.field.playerTop.handCard.size should be (1)
+
+        command.doStep
+
+        controller.field.isEmpty(0,0) should be (false)
+        controller.field.playerTop.handCard.size should be (0)
+
+        command.undoStep
+
+        controller.field.isEmpty(0,0) should be (true)
+        controller.field.playerTop.handCard.size should be (1)
+
+        command.redoStep
+
+        controller.field.isEmpty(0,0) should be (false)
+        controller.field.playerTop.handCard.size should be (0)
       }
-      "undo the step" in {
+      "be able to do,undo and redo Steps from the Bottom Player" in {
+        val controller = new Controller(Field(Vector[Vector[Option[Card]]](),new Sunshine,playerTop,playerBot,0,0))
+        val command = new PlayCardCommand(controller.field,0,0,BOT,0,controller)
 
+        controller.field.isEmpty(0,0) should be (true)
+        controller.field.playerBot.handCard.size should be (1)
+
+        command.doStep
+
+        controller.field.isEmpty(0,0) should be (false)
+        controller.field.playerBot.handCard.size should be (0)
+
+        command.undoStep
+
+        controller.field.isEmpty(0,0) should be (true)
+        controller.field.playerBot.handCard.size should be (1)
+
+        command.redoStep
+
+        controller.field.isEmpty(0,0) should be (false)
+        controller.field.playerBot.handCard.size should be (0)
       }
-      "redo the step" in {
-
-      }
-
     }
   }
 }
