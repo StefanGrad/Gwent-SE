f1294de8d2cb4470d4adcbdede92a99e104625c5
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-01-23 18:47:04 +0100

    Gwent for GwentSpec

--- src/test/scala/de/htwg/se/gwent/GwentSpec.scala
+++ src/test/scala/de/htwg/se/gwent/GwentSpec.scala
@@ -4,10 +4,10 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class GwentSpec extends AnyWordSpec with Matchers{
-
   "The Gwent main class" should {
     "accept text input as argument without readline loop, to test it from command line " in {
       Gwent.main(Array[String]("000_333_c_c_q"))
+      true should be(true)
     }
   }
 }
